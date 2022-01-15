5b2a2aae7825b77e7bb0f78a01d580f9885f6ff4
aliy0012 <alikamal415@yahoo.com>
2022-01-15 14:28:23 -0500

    add header and footer into app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Header from './comonents/Header';
+import Footer from './comonents/Footer';
 
 function App() {
   return (
    
       <>
+      <Header />
+
+      <main>
       <h1>Online Coffee Store</h1>
+      </main>
+      <Footer />
       </>
       
   );
