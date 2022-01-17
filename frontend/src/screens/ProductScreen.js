e6477a45aa6a4198881cab2d513fc267765b0a95
aliy0012 <alikamal415@yahoo.com>
2022-01-16 23:10:08 -0500

    add Product screen component

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,70 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  Col,
+  Row,
+  ListGroup,
+  Image,
+  Card,
+  Button,
+  ListGroupItem,
+} from "react-bootstrap";
+import Rating from "../components/Rating";
+import products from "../products";
 
-const ProductScreen = () => {
+const ProductScreen = (props) => {
+  const product = products.find((p) => p._id === props.match.id);
   return (
-    <div>
-      Product
-    </div>
-  )
-}
+    <>
+      <Link className="btn btn-light my-3" to="/">
+        Go Back
+      </Link>
 
-export default ProductScreen
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroupItem>
+              <h3>{product.name}</h3>
+            </ListGroupItem>
+            <ListGroupItem>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroupItem>
+            <ListGroupItem>Price: ${product.price}</ListGroupItem>
+            <ListGroupItem>Description: {product.description}</ListGroupItem>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroupItem>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>${product.price}</Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Row>
+                  <Col>In Stock:</Col>
+                  <Col>${product.countInStock}</Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Button className="btn-block" type="button">
+                  Add to cart
+                </Button>
+              </ListGroupItem>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default ProductScreen;
