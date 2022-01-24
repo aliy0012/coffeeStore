dd3572e971a8aff3f6723a118a2928a51b94ac5b
aliy0012 <alikamal415@yahoo.com>
2022-01-24 23:31:56 +0000

    add user list screen

---
+++ frontend/src/screens/UserListScreen.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import { Table, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { listUsers, deleteUser } from "../actions/userActions";
+
+export const UserListScreen = () => {
+  return <div></div>;
+};
